56bd4299ee1ec81caa258e2ee7a0adc31139ff15
Omphile-Mphahlele <mphahleleomphile96@gmail.com>
2026-06-22 14:23:56 +0200

    Fixed column grouping for gold.dim_customers

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -47,12 +47,12 @@ ci.cst_id AS customer_id,
 ci.cst_key AS customer_number,
 ci.cst_firstname AS first_name,
 ci.cst_lastname AS last_name,
+la.cntry AS country,
 ci.cst_marital_status AS marital_status,
 CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
 	 ELSE COALESCE(ca.gen, 'n/a')
 END AS gender, --Business Rule: crm data is more accurate than erp data
 ca.bdate AS birthdate,
-la.cntry AS country,
 ci.cst_create_date AS create_date
 FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
@@ -60,6 +60,7 @@ ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid;
 GO
+
 
 IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
 	DROP VIEW gold.fact_sales;
